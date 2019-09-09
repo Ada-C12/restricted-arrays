9ece4c5e9cac5fb0493683c298511facf769966c
brilatimer <brihiramine@gmail.com>
2019-09-09 10:32:31 -0700

    found smallest

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -54,65 +54,74 @@ def find_largest(array, length)
   index = 0
   max = nil
   while (array[index] != nil)
-    if max == nil || array[index] > max = array[index]
-      index += 1 
+    if max == nil || array[index] > max 
+      max = array[index]
     end
-    return max 
+    index += 1 
   end
-  
-  # Finds and returns the smallest integer value in the array
-  # Assumes that the array is not sorted.
-  # Time complexity: ?
-  # Space complexity: ?
-  def find_smallest(array, length)
-    raise NotImplementedError
+  return max 
+end
+
+# Finds and returns the smallest integer value in the array
+# Assumes that the array is not sorted.
+# Time complexity: O(n)
+# Space complexity: O(n)
+def find_smallest(array, length)
+  index = 0
+  min = nil
+  while (array[index] != nil)
+    if min == nil || array[index] < min
+      min = array[index]
+    end
+    index += 1
   end
-  
-  # Reverses the values in the integer array in place
-  # Time complexity: ?
-  # Space complexity: ?
-  def reverse(array, length)
-    raise NotImplementedError
-  end
-  
-  # For an array sorted in ascending order, searches for 'value_to_find'.
-  # Returns true if found, false otherwise.
-  # Time complexity: ?
-  # Space complexity: ?
-  def binary_search(array, length, value_to_find)
-    raise NotImplementedError
-  end
-  
-  # Helper method provided to sort the array in ascending order
-  # Implements selection sort
-  # Time complexity = O(n^2), where n is the number of elements in the array.
-  # To find the correct value for index 0, a total of (n-1) comparisons are needed.
-  # To find the correct value for index 1, a total of (n-2) comparisons are needed.
-  # To find the correct value for index 2, a total of (n-3) comparisons are needed.
-  # and so on ...
-  # To find the correct value for index (n-2), a total of 1 comparisons is needed.
-  # To find the correct value for the last index, a total of 0 comparisons are needed.
-  # Total number of comparisons = (n-1) + (n-2) + ... 3 + 2 + 1
-  #                             = (n * (n-1))/2
-  # This is O(n^2) in Big O terms.
-  # Space complexity = constant or O(1) since the additional storage needed,
-  # does not depend on input array size.
-  def sort(array, length)
-    length.times do |index| # outer loop - n elements
-      min_index = index # assume index is where the next minimally value is
-      temp_index = index+1 # compare with values at index+1 to length-1
-      while temp_index < length # inner loop - n-1 elements
-        if array[temp_index] < array[min_index] # found a new minimum, update min_index
-          min_index = temp_index
-        end
-        temp_index += 1 # move to next index
+  return min
+end
+
+# Reverses the values in the integer array in place
+# Time complexity: ?
+# Space complexity: ?
+def reverse(array, length)
+  raise NotImplementedError
+end
+
+# For an array sorted in ascending order, searches for 'value_to_find'.
+# Returns true if found, false otherwise.
+# Time complexity: ?
+# Space complexity: ?
+def binary_search(array, length, value_to_find)
+  raise NotImplementedError
+end
+
+# Helper method provided to sort the array in ascending order
+# Implements selection sort
+# Time complexity = O(n^2), where n is the number of elements in the array.
+# To find the correct value for index 0, a total of (n-1) comparisons are needed.
+# To find the correct value for index 1, a total of (n-2) comparisons are needed.
+# To find the correct value for index 2, a total of (n-3) comparisons are needed.
+# and so on ...
+# To find the correct value for index (n-2), a total of 1 comparisons is needed.
+# To find the correct value for the last index, a total of 0 comparisons are needed.
+# Total number of comparisons = (n-1) + (n-2) + ... 3 + 2 + 1
+#                             = (n * (n-1))/2
+# This is O(n^2) in Big O terms.
+# Space complexity = constant or O(1) since the additional storage needed,
+# does not depend on input array size.
+def sort(array, length)
+  length.times do |index| # outer loop - n elements
+    min_index = index # assume index is where the next minimally value is
+    temp_index = index+1 # compare with values at index+1 to length-1
+    while temp_index < length # inner loop - n-1 elements
+      if array[temp_index] < array[min_index] # found a new minimum, update min_index
+        min_index = temp_index
       end
-      if min_index != index # next minimum value is not at current index, swap
-        temp = array[min_index]
-        array[min_index] = array[index]
-        array[index] = temp
-      end
+      temp_index += 1 # move to next index
+    end
+    if min_index != index # next minimum value is not at current index, swap
+      temp = array[min_index]
+      array[min_index] = array[index]
+      array[index] = temp
     end
   end
-  ## --- END OF METHODS ---
-  
+end
+## --- END OF METHODS ---
